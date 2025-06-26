63f3edc1dc712600fabc3c7e8a69827b1b9caac8
Yiannis Vavouranakis <ivavouranakis@gmail.com>
2025-06-27 01:39:15 +0300

    player creation and player page

--- js/app.js
+++ js/app.js
@@ -213,20 +213,215 @@
             "action": "4 *"
         }
     ];
-    let selectedGlyphs=[];
-    let selectedWords=[];
-    document.addEventListener("DOMContentLoaded", function() {
-        glyphs.forEach(addGlyphOption);
-        document.getElementById("glyphInput").addEventListener("input", glyphInputChange);
-        words.forEach(w=>{document.body.append(addWordItem(w))});
+    let players=[];
+    let selectedPlayer=null;
+    document.addEventListener("DOMContentLoaded", () => {
+        let app = document.getElementById("app");
+        buildGamePage(app);
     });
-    const addGlyphOption = function(g){
-        const datalist = document.getElementById("glyphs");
-        const option = document.createElement("option");
-        option.value = g.name;
-        datalist.appendChild(option);
+    const buildGamePage = (app) => {
+        clearPage(app);
+        createPlayerInput(app);
+        createStartButton(app);
+        InitializePlayerList(app);
+    };
+    const buildPlayerPage=(app)=>{
+        clearPage(app);
+        createPlayerSelectionList(app);
+        createBaseScoreDisplay(app);
+        createFinalScoreDisplay(app);
+        createGlyphInput(app);
+        createGlyphList(app);
+        createPlayerGlyphTable(app);
+        createWordList(app);
+        updateScoreDisplay();
+        buildGlyphTable();
+        updateWordDisplay();
+    };
+    const clearPage = (app)=>{
+        app.innerHTML="";
+    };
+    const createPlayerInput=(app)=>{
+        let playerInput=app.appendChild(document.createElement("input"));
+        playerInput.setAttribute("id", "playerInput");
+        playerInput.setAttribute("placeholder", "Enter player name");
+
+        let playerNameButton = app.appendChild(document.createElement("button"));
+        playerNameButton.textContent="+";
+        playerNameButton.addEventListener("click", addPlayer);
+    };
+    const createStartButton=(app)=>{
+        let startButtonDiv=app.appendChild(document.createElement("div"));
+        let startButton=document.createElement("button");
+        startButton.textContent="Start Game";
+        startButtonDiv.appendChild(startButton);
+        startButton.addEventListener("click", startGame);
+    };
+    const InitializePlayerList=(app)=>{
+        let playerList=document.createElement("ul");
+        app.appendChild(playerList);
+        playerList.setAttribute("id", "playerList");
+        players=[];
+    };
+    const createBaseScoreDisplay=(app)=>{
+        let baseScoreDiv=app.appendChild(document.createElement("div"));
+        baseScoreDiv.textContent="Base Score: ";
+        let baseScore=baseScoreDiv.appendChild(document.createElement("span"));
+        baseScore.setAttribute("id", "baseScore");
+    };
+    const createFinalScoreDisplay=(app)=>{
+        let finalScoreDiv=app.appendChild(document.createElement("div"));
+        finalScoreDiv.textContent="Final Score: ";
+        let finalScore=finalScoreDiv.appendChild(document.createElement("span"));
+        finalScore.setAttribute("id", "finalScore");
+    };
+    const createGlyphInput=(app)=>{
+        let glyphInput=app.appendChild(document.createElement("input"));
+        glyphInput.setAttribute("list", "glyphs");
+        glyphInput.setAttribute("name", "glyph");
+        glyphInput.setAttribute("id", "glyphInput");
+        glyphInput.addEventListener("input", glyphInputChange);
+    };
+    const createGlyphList=(app)=>{
+        let glyphsList=app.appendChild(document.createElement("datalist"));
+        glyphsList.setAttribute("id", "glyphs");
+        glyphs.forEach(g=>{if (!players.some(p=>p.glyphs.some(pg=>pg.name==g.name))) addGlyphOption(g, glyphsList);});
+    };
+    const createPlayerGlyphTable=(app)=>{
+        let glyphTable=app.appendChild(document.createElement("table"));
+        glyphTable.setAttribute("id", "glyphTable");
+    };  
+    const createWordList=(app)=>{
+        words.forEach(w=>{app.appendChild(createWordItem(w))});
+    };
+    const createPlayerSelectionList=(app)=>{
+        let playerList=document.createElement("select");
+        app.appendChild(playerList);
+        playerList.setAttribute("id", "playerList");
+        players.forEach(p=>{
+            let player=document.createElement("option");
+            player.value=p.name;
+            player.textContent=p.name;
+            playerList.appendChild(player);
+        });
+        playerList.value=selectedPlayer.name;
+        playerList.addEventListener("change", playerListChange);
+    };
+    const startGame=(e)=>{
+        if (players.length==0) return;
+        selectedPlayer=players[0];
+        buildPlayerPage(app);
+    };
+    const addPlayer=(e)=>{
+        let playerName=document.getElementById("playerInput").value;
+        if (playerName.trim()!="") {
+            players.push({name:playerName, glyphs:[], words:[]});
+            document.getElementById("playerInput").value="";
+            createPlayerList();
+        }
+    };
+    const playerListChange=(e)=>{
+        selectedPlayer=players.find(p=>p.name==e.target.value);
+        buildPlayerPage(app);
+    };
+    const glyphInputChange=(e) => {
+        const input = e.target;
+        const selected = glyphs.find(g => g.name === input.value);
+        if (!selected) return;
+        selectedPlayer.glyphs.push(selected);
+        updateScoreDisplay();
+        buildGlyphTable();
+        updateWordDisplay();
+        input.value='';
+        removeGlyphOption(selected);
+        players[players.findIndex(p=>p.name==selectedPlayer.name)]=selectedPlayer;
+    };
+    const buildGlyphTable=()=>{
+        const glyphTable = document.getElementById("glyphTable");
+        glyphTable.innerHTML="";
+        let currentRow=null;
+        let cellCount = 0;
+        selectedPlayer.glyphs.forEach(g=>{
+            if (glyphTable.rows.length>0)
+                currentRow = glyphTable.rows[glyphTable.rows.length - 1];
+            else 
+                currentRow = glyphTable.insertRow();
+            cellCount=currentRow.cells.length;
+            if (cellCount % 5 === 0 && cellCount>0)
+                currentRow = glyphTable.insertRow();    
+            buildGlyphCell(g, glyphTable);
+            cellCount++;
+        });
+    };
+    const calculateBaseScore=()=>{ 
+        return selectedPlayer.glyphs.map(g=> {return g.score}).reduce((a, c) => {return a + c}, 0) 
+    };
+    const updateScoreDisplay=()=>{
+        document.getElementById("baseScore").innerHTML=calculateBaseScore();
+        document.getElementById("finalScore").innerHTML=calculateFinalScore();
+    };
+    const createPlayerList=()=>{
+        document.getElementById("playerList").innerHTML="";
+        players.forEach(p=>{
+            let player=document.createElement("li");
+            player.textContent=p.name;
+            document.getElementById("playerList").appendChild(player);
+        });
+    };
+    const allPossibleCombos = ()=>{
+        const arr = words.filter(w => w.glyphs.every(wg => selectedPlayer.glyphs.some(g=>g.name==wg)));
+        const total = Math.pow(2, arr.length);
+        let res = [];
+        for (let i = 0; i < total; i++)
+        {
+            let comb = [];
+            for (let j = 0; j < arr.length; j++){
+                if ((i & Math.pow(2,j))){ 
+                    comb.push(arr[j]);
+                }
+            }
+            if (comb.length) {
+                res.push(comb);
+            }
+        }
+        return res.filter(comboList =>
+        {
+            for(const word of comboList)
+            {
+                if (comboList.some(t => t.name != word.name && t.glyphs.some(tg => word.glyphs.some(wg=>wg==tg)))) return false;
+            }
+            return true;
+        });
+    };
+    const calculateFinalScore=()=>{
+        var score = calculateBaseScore();
+        selectedPlayer.words=[];
+        const allpossible = allPossibleCombos();
+        allpossible.forEach(combo=>
+        {
+            var tempScore = finalScoreCandidate(combo);
+            if (tempScore > score)
+            {
+                score = tempScore;
+                selectedPlayer.words = combo;
+            }
+        });
+        return score;
+    };
+    const updateWordDisplay=()=>{
+        document.querySelectorAll(".word").forEach(g=>g.className="word");
+        document.querySelectorAll(".glyph").forEach(g=>g.className="glyph unset");
+        selectedPlayer.glyphs.forEach(g => { document.querySelectorAll("[data-glyph='"+g.name+"']").forEach(el=>el.className="glyph set"); });
+        selectedPlayer.words.forEach(w => {document.getElementById(w.glyphs.join("")).className="word selected"});
+        players.filter(p=>p.name!=selectedPlayer.name).forEach(p=>{
+            p.glyphs.forEach(g=>{
+                document.querySelectorAll("[data-glyph='"+g.name+"']").forEach(el=>{
+                    el.closest(".word").className="word lost";
+                });
+            });
+        });
     }
-    const addWordItem = function(w){
+    const createWordItem = (w)=>{
         let wordTable = document.createElement("table")
         wordTable.className="word";
         wordTable.setAttribute("id",w.glyphs.join(""));
@@ -246,8 +441,7 @@
         }
         return wordTable;
     };
-    const removeGlyphOption=function(g)
-    {
+    const removeGlyphOption=(g)=>{
         const options = document.querySelectorAll("#glyphs option");
         for(i=0;i<options.length;++i)
         {
@@ -257,66 +451,12 @@
             }
         }
     };
-    const glyphInputChange=function(e) {
-        const input = e.target;
-        const selected = glyphs.find(g => g.name === input.value);
-        if (!selected) return;
-        selectedGlyphs.push(selected);
-        updateScoreDisplay();
-        buildGlyphTable();
-        updateWordDisplay();
-        input.value='';
-        removeGlyphOption(selected);
-    };
-    const buildGlyphTable=function(){
-    const glyphTable = document.getElementById("glyphTable");
-    glyphTable.innerHTML="";
-    let currentRow=null;
-    let cellCount = 0;
-    selectedGlyphs.forEach(g=>{
-        if (glyphTable.rows.length>0)
-            currentRow = glyphTable.rows[glyphTable.rows.length - 1];
-        else 
-            currentRow = glyphTable.insertRow();
-        cellCount=currentRow.cells.length;
-        if (cellCount % 5 === 0 && cellCount>0)
-            currentRow = glyphTable.insertRow();    
-        buildGlyphCell(g, glyphTable);
-        cellCount++;
-    });
-    };
-    const buildGlyphCell=function(glyph, table){
-    const cell = table.rows[table.rows.length - 1].insertCell();
-    const cellTable = document.createElement("table")
-    cellTable.className="glyphs";
-    cell.appendChild(cellTable);
-    let r = cellTable.insertRow();
-    let c = r.insertCell();
-    c.className="name"
-    c.textContent=glyph.name;
-    r = cellTable.insertRow();
-    c = r.insertCell();
-    c.className="glyph"
-    c.textContent=String.fromCodePoint(glyph.sign);
-    r = cellTable.insertRow();
-    c = r.insertCell();
-    c.className="score"
-    c.textContent=glyph.score;
-    }
-    const calculateBaseScore=function(){
-        return selectedGlyphs.map(g=> {return g.score}).reduce((a, c) => {return a + c}, 0)
-    }
-    const updateScoreDisplay=function()
-    {
-        document.getElementById("baseScore").innerHTML=calculateBaseScore();
-        document.getElementById("finalScore").innerHTML=calculateFinalScore();
-    }
-    const replaceCounter=function(rpnToken) {
+    const replaceCounter=(rpnToken) =>{
         const match = rpnToken.match(/\[c(\d+)\]/gi);
         if (!match) return rpnToken;
-        return selectedGlyphs.filter(t => t.score == parseInt(rpnToken.match(/\d/), 10)).map(i=>i.score).reduce((a, c) => {return a + c}, 0);
+        return selectedPlayer.glyphs.filter(t => t.score == parseInt(rpnToken.match(/\d/), 10)).map(i=>i.score).reduce((a, c) => {return a + c}, 0);
     };
-    const replaceCounters=function(scoreString){
+    const replaceCounters=(scoreString)=>{
         return scoreString.split(/(\s+)/).filter(i=>i.trim()!="").map(replaceCounter).join(" ");
     };
     const rpn = (input) => {
@@ -354,56 +494,30 @@
         }
         return stack.pop();
     };
-    const finalScoreCandidate=function(wordCombo){
+    const finalScoreCandidate=(wordCombo)=>{
         return rpn(replaceCounters(calculateBaseScore()+" "+wordCombo.map(w => w.action).join(" ")));
-    }
-    const allPossibleCombos = function()
-    {
-        const arr = words.filter(w => w.glyphs.every(wg => selectedGlyphs.some(g=>g.name==wg)));
-        const total = Math.pow(2, arr.length);
-        let res = [];
-        for (let i = 0; i < total; i++)
-        {
-            let comb = [];
-            for (let j = 0; j < arr.length; j++){
-                if ((i & Math.pow(2,j))){ 
-                    comb.push(arr[j]);
-                }
-            }
-            if (comb.length) {
-                res.push(comb);
-            }
-        }
-        return res.filter(comboList =>
-        {
-            for(const word of comboList)
-            {
-                if (comboList.some(t => t.name != word.name && t.glyphs.some(tg => word.glyphs.some(wg=>wg==tg)))) return false;
-            }
-            return true;
-        });
     };
-    const calculateFinalScore=function()
-    {
-        var score = calculateBaseScore();
-        selectedWords=[];
-        const allpossible = allPossibleCombos();
-        allpossible.forEach(combo=>
-        {
-            var tempScore = finalScoreCandidate(combo);
-            if (tempScore > score)
-            {
-                score = tempScore;
-                selectedWords = combo;
-            }
-        });
-        return score;
+    const addGlyphOption = (glyph, datalist)=>{
+        const option = document.createElement("option");
+        option.value = glyph.name;
+        datalist.appendChild(option);
     };
-    const updateWordDisplay=function(){
-        document.querySelectorAll(".word").forEach(g=>g.className="word");
-        document.querySelectorAll(".glyph").forEach(g=>g.className="glyph unset");
-        selectedGlyphs.forEach(g => { document.querySelectorAll("[data-glyph='"+g.name+"']").forEach(el=>el.className="glyph set"); });
-        selectedWords.forEach(w => {document.getElementById(w.glyphs.join("")).className="word selected"});
-    }
-    
+    const buildGlyphCell=(glyph, table)=>{
+        const cell = table.rows[table.rows.length - 1].insertCell();
+        const cellTable = document.createElement("table")
+        cellTable.className="glyphs";
+        cell.appendChild(cellTable);
+        let r = cellTable.insertRow();
+        let c = r.insertCell();
+        c.className="name"
+        c.textContent=glyph.name;
+        r = cellTable.insertRow();
+        c = r.insertCell();
+        c.className="glyph"
+        c.textContent=String.fromCodePoint(glyph.sign);
+        r = cellTable.insertRow();
+        c = r.insertCell();
+        c.className="score"
+        c.textContent=glyph.score;
+    };
 })();
